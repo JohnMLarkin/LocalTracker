5b4757e5049626d2441029885952b6ab9f9261d6
John Larkin <jlarkin@whitworth.edu>
2019-07-02 08:23:31 -0700

    Setup main node packages and main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+
+var mainWindow = null
+
+function createWindow () {
+  var windowOptions = {
+    width: 1900,
+    minWidth: 680,
+    height: 1000,
+    webPreferences: {nodeIntegration: true}
+  }
+
+  mainWindow = new BrowserWindow(windowOptions)
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, '/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
